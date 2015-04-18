7d42a7df35018f2564a1d124649257a4d3ce7d4f
Kyle Kersey <kylelk@comcast.net>
2015-04-18 19:05:02 +0000

    updated hello world example to use Linux system calls

--- hello.s
+++ hello.s
@@ -1,12 +1,18 @@
-.global main
-
-message:
-    .asciz "hello world\n"
-    .align 4
-
-main:
-    ldr r0, =message
-    bl printf
-
+@ can be compiled using:  gcc -nostdlib hello.s -o hello
+@ can also be compiled using:  as hello.s -o hello.o ; ld hello.o -o hello
+.global _start
+_start:
+    mov r0, #1
+    ldr r1, =message
+    ldr r2, =len
+    mov r7, #4
+    swi 0
+    
+    mov r0, #0
     mov r7, #1
     swi 0
+
+.data
+message:
+    .ascii "hello world\n"
+len = .-message
